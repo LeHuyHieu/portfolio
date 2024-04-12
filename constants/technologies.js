ce6737dbebb1f4931ae11350f40e8957bc1df16b
LeHuyHieu <126578220+LeHuyHieu@users.noreply.github.com>
2024-04-12 18:38:07 +0700

    update

--- constants/technologies.js
+++ constants/technologies.js
@@ -7,6 +7,14 @@ import Laravel from "public/assets/svg/laravel.svg";
 import Mysql from "public/assets/svg/mysql.svg";
 import Php from "public/assets/svg/php.svg";
 import Git from "public/assets/svg/git.svg";
+import Postman from "public/assets/svg/postman.svg";
+import Figma from "public/assets/svg/figma.svg";
+import Vue from "public/assets/svg/vue.svg";
+import Angular from "public/assets/svg/angular.svg";
+import Scss from "public/assets/svg/scss.svg";
+import Vscode from "public/assets/svg/vs-code.svg";
+import Cursor from "public/assets/svg/cursor.svg";
+import Phpstorm from "public/assets/svg/phpstorm.svg";
 
 export const TECHNOLOGIES = [
 	{
@@ -17,6 +25,9 @@ export const TECHNOLOGIES = [
 			{ name: "JS", icon: <IoLogoJavascript size={32} /> },
 			{ name: "Jquery", icon: <Jquery width={32} size={32} /> },
 			{ name: "Bootstrap", icon: <BsBootstrap size={32} /> },
+			{ name: "VueJS", icon: <Vue width={32} size={32} /> },
+			{ name: "Angular", icon: <Angular width={32} size={32} /> },
+			{ name: "Scss", icon: <Scss width={32} size={32} /> },
 		]
 	},
 	{
@@ -31,6 +42,16 @@ export const TECHNOLOGIES = [
 		category: "Other",
 		items: [
 			{ name: "Git", icon: <Git width={32} size={32} /> },
+			{ name: "Postman", icon: <Postman width={32} size={32} /> },
+			{ name: "Figma", icon: <Figma width={32} size={32} /> },
+		]
+	},
+	{
+		category: "Editer",
+		items: [
+			{ name: "VsCode", icon: <Vscode width={32} size={32} /> },
+			{ name: "Cursor", icon: <Cursor width={32} size={32} /> },
+			{ name: "PhpStorm", icon: <Phpstorm width={32} size={32} /> },
 		]
 	},
 ];
